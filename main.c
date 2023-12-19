1140048be018ab3737417bae753b39ddd7c411bb
pdlmn <jseldardw@gmail.com>
2023-12-19 21:03:28 +0100

    feat: added basic circular linked list functions

---
+++ main.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 19:15:33 by emuminov          #+#    #+#             */
+/*   Updated: 2023/12/19 20:48:47 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+typedef struct s_node
+{
+	struct s_node	*next;
+	struct s_node	*prev;
+	int				value;
+}	t_node;
+
+typedef struct s_list
+{
+	t_node	*head;
+	t_node	*tail;
+}	t_list;
+
+void	list_free(t_list *lst)
+{
+	t_node	*curr;
+	t_node	*next;
+
+	if (!lst)
+		return ;
+	curr = lst->head;
+	next = lst->head->next;
+	while (curr)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+		if (curr == lst->head)
+			break ;
+	}
+	free(lst);
+}
+
+t_list	*list_append(int num, t_list *lst)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = num;
+	if (!lst->head)
+	{
+		node->next = node;
+		node->prev = node;
+		lst->head = node;
+		lst->tail = node;
+		return (lst);
+	}
+	node->next = lst->head;
+	node->prev = lst->tail;
+	lst->tail->next = node;
+	lst->tail = node;
+	return (lst);
+}
+
+t_list	*list_init(int nums_len, char **nums)
+{
+	t_list	*lst;
+	int		i;
+
+	lst = malloc(sizeof(t_list));
+	if (!lst)
+		return (NULL);
+	lst->head = NULL;
+	lst->tail = NULL;
+	i = 0;
+	while (i < nums_len)
+	{
+		if (!list_append(atoi(nums[i]), lst))
+		{
+			list_free(lst);
+			return (NULL);
+		}
+		i++;
+	}
+	return (lst);
+}
+
+// TODO: implement circular linked list
+// TODO: implement swapping functions
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	t_list *lst = list_init(argc - 1, &argv[1]);
+	t_node *curr = lst->head;
+	while (curr) {
+		printf("%d\n", curr->value);
+		curr = curr->next;
+		if (curr == lst->head)
+			break;
+	}
+}
