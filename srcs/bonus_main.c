729a80771b1af5b1e4a582dec3afd3944181c74e
Eldar Muminov <emuminov@z4r8p1.42lyon.fr>
2024-02-22 12:52:13 +0100

    feat: added working bonus

--- srcs/bonus_main.c
+++ srcs/bonus_main.c
@@ -6,53 +6,51 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:25:58 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/22 10:17:11 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/22 12:49:33 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-#include <stdio.h>
-#include <string.h>
 
 int	is_operation(char *str)
 {
-	return (strcmp(str, "sa\n") == 0
-		|| strcmp(str, "sb\n") == 0
-		|| strcmp(str, "ss\n") == 0
-		|| strcmp(str, "ra\n") == 0
-		|| strcmp(str, "rb\n") == 0
-		|| strcmp(str, "rr\n") == 0
-		|| strcmp(str, "rra\n") == 0
-		|| strcmp(str, "rrb\n") == 0
-		|| strcmp(str, "rrr\n") == 0
-		|| strcmp(str, "pb\n") == 0
-		|| strcmp(str, "pa\n") == 0
+	return (ft_strcmp(str, "sa\n") == 0
+		|| ft_strcmp(str, "sb\n") == 0
+		|| ft_strcmp(str, "ss\n") == 0
+		|| ft_strcmp(str, "ra\n") == 0
+		|| ft_strcmp(str, "rb\n") == 0
+		|| ft_strcmp(str, "rr\n") == 0
+		|| ft_strcmp(str, "rra\n") == 0
+		|| ft_strcmp(str, "rrb\n") == 0
+		|| ft_strcmp(str, "rrr\n") == 0
+		|| ft_strcmp(str, "pb\n") == 0
+		|| ft_strcmp(str, "pa\n") == 0
 	);
 }
 
 void	do_operation(char *op, t_stacks *stacks)
 {
-	if (strcmp(op, "sa\n") == 0)
+	if (ft_strcmp(op, "sa\n") == 0)
 		sa(1, stacks);
-	else if (strcmp(op, "sb\n") == 0)
+	else if (ft_strcmp(op, "sb\n") == 0)
 		sb(1, stacks);
-	else if (strcmp(op, "ss\n") == 0)
+	else if (ft_strcmp(op, "ss\n") == 0)
 		ss(1, stacks);
-	else if (strcmp(op, "ra\n") == 0)
+	else if (ft_strcmp(op, "ra\n") == 0)
 		ra(1, stacks);
-	else if (strcmp(op, "rb\n") == 0)
+	else if (ft_strcmp(op, "rb\n") == 0)
 		rb(1, stacks);
-	else if (strcmp(op, "rr\n") == 0)
+	else if (ft_strcmp(op, "rr\n") == 0)
 		rr(1, stacks);
-	else if (strcmp(op, "rra\n") == 0)
+	else if (ft_strcmp(op, "rra\n") == 0)
 		rra(1, stacks);
-	else if (strcmp(op, "rrb\n") == 0)
+	else if (ft_strcmp(op, "rrb\n") == 0)
 		rrb(1, stacks);
-	else if (strcmp(op, "rrr\n") == 0)
+	else if (ft_strcmp(op, "rrr\n") == 0)
 		rrr(1, stacks);
-	else if (strcmp(op, "pa\n") == 0)
+	else if (ft_strcmp(op, "pa\n") == 0)
 		pa(1, stacks);
-	else if (strcmp(op, "pb\n") == 0)
+	else if (ft_strcmp(op, "pb\n") == 0)
 		pb(1, stacks);
 }
 
@@ -84,7 +82,7 @@ int	main(int argc, char **argv)
 		handle_error(stacks.stack_a, NULL, NULL, 1);
 	receive_operations(&stacks);
 	if (list_is_sorted(stacks.stack_a))
-		printf("KO\n");
+		ft_putstr_fd("OK\n", 1);
 	else
-		printf("KO\n");
+		ft_putstr_fd("KO\n", 1);
 }
