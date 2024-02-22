34726b737ad76446856392759152794d6f5fdb3d
Eldar Muminov <emuminov@z4r8p1.42lyon.fr>
2024-02-22 17:37:04 +0100

    fix: fixed the annoying rr bug

--- srcs/bonus_main.c
+++ srcs/bonus_main.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:25:58 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/22 16:39:52 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/22 17:32:39 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,43 @@
 
 int	is_operation(char *str)
 {
-	return (ft_strcmp(str, "sa\n") == 0
-		|| ft_strcmp(str, "sb\n") == 0
-		|| ft_strcmp(str, "ss\n") == 0
-		|| ft_strcmp(str, "ra\n") == 0
-		|| ft_strcmp(str, "rb\n") == 0
-		|| ft_strcmp(str, "rr\n") == 0
-		|| ft_strcmp(str, "rra\n") == 0
-		|| ft_strcmp(str, "rrb\n") == 0
-		|| ft_strcmp(str, "rrr\n") == 0
-		|| ft_strcmp(str, "pb\n") == 0
-		|| ft_strcmp(str, "pa\n") == 0
+	return (ft_strncmp(str, "sa", 2) == 0
+		|| ft_strncmp(str, "sb", 2) == 0
+		|| ft_strncmp(str, "ss", 2) == 0
+		|| ft_strncmp(str, "ra", 2) == 0
+		|| ft_strncmp(str, "rb", 2) == 0
+		|| ft_strncmp(str, "rr", 2) == 0
+		|| ft_strncmp(str, "rra", 3) == 0
+		|| ft_strncmp(str, "rrb", 3) == 0
+		|| ft_strncmp(str, "rrr", 3) == 0
+		|| ft_strncmp(str, "pb", 2) == 0
+		|| ft_strncmp(str, "pa", 2) == 0
 	);
 }
 
 void	do_operation(char *op, t_stacks *stacks)
 {
-	if (ft_strcmp(op, "sa\n") == 0)
+	if (ft_strncmp(op, "sa", 2) == 0)
 		sa(1, stacks);
-	else if (ft_strcmp(op, "sb\n") == 0)
+	else if (ft_strncmp(op, "sb", 2) == 0)
 		sb(1, stacks);
-	else if (ft_strcmp(op, "ss\n") == 0)
+	else if (ft_strncmp(op, "ss", 2) == 0)
 		ss(1, stacks);
-	else if (ft_strcmp(op, "ra\n") == 0)
-		ra(1, stacks);
-	else if (ft_strcmp(op, "rb\n") == 0)
-		rb(1, stacks);
-	else if (ft_strcmp(op, "rr\n") == 0)
-		rr(1, stacks);
-	else if (ft_strcmp(op, "rra\n") == 0)
+	else if (ft_strncmp(op, "rra", 3) == 0)
 		rra(1, stacks);
-	else if (ft_strcmp(op, "rrb\n") == 0)
+	else if (ft_strncmp(op, "rrb", 3) == 0)
 		rrb(1, stacks);
-	else if (ft_strcmp(op, "rrr\n") == 0)
+	else if (ft_strncmp(op, "rrr", 3) == 0)
 		rrr(1, stacks);
-	else if (ft_strcmp(op, "pa\n") == 0)
+	else if (ft_strncmp(op, "ra", 2) == 0)
+		ra(1, stacks);
+	else if (ft_strncmp(op, "rb", 2) == 0)
+		rb(1, stacks);
+	else if (ft_strncmp(op, "rr", 2) == 0)
+		rr(1, stacks);
+	else if (ft_strncmp(op, "pa", 2) == 0)
 		pa(1, stacks);
-	else if (ft_strcmp(op, "pb\n") == 0)
+	else if (ft_strncmp(op, "pb", 2) == 0)
 		pb(1, stacks);
 }
 
@@ -59,8 +59,6 @@ void	receive_operations(t_stacks *stacks)
 	char	*op;
 
 	op = get_next_line(STDIN_FILENO);
-	if (!op)
-		handle_error(stacks->stack_a, stacks->stack_b, NULL, 1);
 	while (op)
 	{
 		if (is_operation(op))
@@ -73,8 +71,6 @@ void	receive_operations(t_stacks *stacks)
 		}
 		free(op);
 		op = get_next_line(STDIN_FILENO);
-		if (!op)
-			handle_error(stacks->stack_a, stacks->stack_b, NULL, 1);
 	}
 	free(op);
 }
@@ -82,15 +78,17 @@ void	receive_operations(t_stacks *stacks)
 int	main(int argc, char **argv)
 {
 	t_stacks	stacks;
+	int			l;
 
 	if (argc == 1)
 		return (EXIT_SUCCESS);
 	stacks.stack_a = parse(argc, argv);
 	stacks.stack_b = list_create(0, NULL);
+	l = stacks.stack_a->length;
 	if (!stacks.stack_b)
 		handle_error(stacks.stack_a, NULL, NULL, 1);
 	receive_operations(&stacks);
-	if (list_is_sorted(stacks.stack_a))
+	if (list_is_sorted(stacks.stack_a) && l == stacks.stack_a->length)
 		ft_putstr_fd("OK\n", STDOUT_FILENO);
 	else
 		ft_putstr_fd("KO\n", STDOUT_FILENO);
